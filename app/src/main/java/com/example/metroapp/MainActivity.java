620b375a30aedb69788b949c812b7eb072cef17c
shashank <shekhar.shashank764@gmail,com>
2019-07-18 18:26:54 +0530

    Added stations list

--- app/src/main/java/com/example/metroapp/MainActivity.java
+++ app/src/main/java/com/example/metroapp/MainActivity.java
@@ -7,21 +7,19 @@ import android.widget.AutoCompleteTextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String[] Stations = new String[]
-            {
-                    "Vaishali", "Kaushambi", "Anand Vihar", "Karkarduma", "Preet Vihar", "Nirman Vihar", "Laxmi Nagar", "Yamuna Bank"
-            };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        String[] stations=  getResources().getStringArray(R.array.stations);
+
         AutoCompleteTextView src = findViewById(R.id.srchome);
-        ArrayAdapter<String> srcadapter = new ArrayAdapter<String>( this, android.R.layout.simple_list_item_1, Stations);
+        ArrayAdapter<String> srcadapter = new ArrayAdapter<String>( this, android.R.layout.simple_list_item_1, stations);
         src.setAdapter(srcadapter);
 
         AutoCompleteTextView dest = findViewById(R.id.desthome);
-        ArrayAdapter<String> destadapter = new ArrayAdapter<String>( this, android.R.layout.simple_list_item_1, Stations);
+        ArrayAdapter<String> destadapter = new ArrayAdapter<String>( this, android.R.layout.simple_list_item_1, stations);
         dest.setAdapter(destadapter);
     }
 }
