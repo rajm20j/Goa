add79b7ea0086c6893d625d8caa0db39188c6332
shashank <shekhar.shashank764@gmail,com>
2019-07-18 18:01:54 +0530

    2 search boxes

--- app/src/main/java/com/example/metroapp/MainActivity.java
+++ app/src/main/java/com/example/metroapp/MainActivity.java
@@ -2,12 +2,26 @@ package com.example.metroapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String[] Stations = new String[]
+            {
+                    "Vaishali", "Kaushambi", "Anand Vihar", "Karkarduma", "Preet Vihar", "Nirman Vihar", "Laxmi Nagar", "Yamuna Bank"
+            };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AutoCompleteTextView src = findViewById(R.id.srchome);
+        ArrayAdapter<String> srcadapter = new ArrayAdapter<String>( this, android.R.layout.simple_list_item_1, Stations);
+        src.setAdapter(srcadapter);
+
+        AutoCompleteTextView dest = findViewById(R.id.desthome);
+        ArrayAdapter<String> destadapter = new ArrayAdapter<String>( this, android.R.layout.simple_list_item_1, Stations);
+        dest.setAdapter(destadapter);
     }
 }
