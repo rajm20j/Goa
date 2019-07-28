0eee2aba2ce6f786a77dd32aa4f309516eb9e915
Rishabh <rmrishabh292@gmail.com>
2019-07-28 19:43:58 +0530

    Added paytm recharge, zoomable map and boht zyada mehnat lagi h bhai, mazak nai kr rha, literally 11 ghnte baithe h aaj Ro gye h esse errors dekhe h main raat bhar jaga hun, shanky 2 ghanta soya h, abhi ghar jaana h mujhe issi neend main gaadi chala kr, SOCH BHAI SOCH

---
+++ app/src/main/java/com/example/metroapp/RouteActivity.java
@@ -0,0 +1,30 @@
+package com.example.metroapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class RouteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_route);
+
+        Intent intRoute = getIntent();
+        String src = intRoute.getStringExtra("SRC");
+        String dest = intRoute.getStringExtra("DEST");
+
+        TextView source = (TextView) findViewById(R.id.route_src);
+        source.setText(src);
+
+        TextView destination = (TextView) findViewById(R.id.route_dest);
+        destination.setText(dest);
+
+    }
+}
+
