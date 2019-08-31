b57cf5f8a4e51bfe2affbe1e61bed9d2f00ba5bc
Rishabh <rmrishabh292@gmail.com>
2019-08-31 15:54:27 +0530

    Added paytm recharge, zoomable map and boht zyada mehnat lagi h bhai, mazak nai kr rha, literally 11 ghnte baithe h aaj Ro gye h esse errors dekhe h main raat bhar jaga hun, shanky 2 ghanta soya h, abhi ghar jaana h mujhe issi neend main gaadi chala kr, SOCH BHAI SOCH
    Added all paths, have to select one path now based on the distance and also make a full fledged data base reflecting the actual Delhi Metro.
    
    Q - Can we make mumbai, hyderabad and bangalore metro apps using this also ?

--- app/src/main/java/com/example/metroapp/RouteActivity.java
+++ app/src/main/java/com/example/metroapp/RouteActivity.java
@@ -28,14 +28,16 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import android.util.Log;
 
 public class RouteActivity extends AppCompatActivity {
-    ArrayList<Integer> stationList= new ArrayList<>();
-    ArrayList<Integer> connectingList= new ArrayList<>();
-    ArrayList<Integer> terminalList= new ArrayList<>();
-    ArrayList<Float> distanceList= new ArrayList<>();
-    ArrayList<String> colorList= new ArrayList<>();
-    Queue <Integer> sourceq = new LinkedList<Integer>();
-    Queue <Integer> destq = new LinkedList<Integer>();
-    HashMap<Integer,Integer> visited = new HashMap<Integer, Integer>();
+    //ArrayList<Integer> stationList= new ArrayList<>();
+    //ArrayList<Integer> connectingList= new ArrayList<>();
+    //ArrayList<Integer> terminalList= new ArrayList<>();
+    //ArrayList<Float> distanceList= new ArrayList<>();
+    //ArrayList<String> colorList= new ArrayList<>();
+    //Queue <Integer> sourceq = new LinkedList<Integer>();
+    //Queue <Integer> destq = new LinkedList<Integer>();
+    //HashMap<Integer,Integer> visited = new HashMap<Integer, Integer>();
+
+    ArrayList <Integer> finalPath = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,8 +48,9 @@ public class RouteActivity extends AppCompatActivity {
         String src = intRoute.getStringExtra("SRC");
         String dest = intRoute.getStringExtra("DEST");
         Integer srcint=0;
+        double shortestDist = 99999999.0;
         Integer destint=0;
-        Integer u1,v1,stationNo=1;
+        Integer u1,v1,stationNo=-1;
 
        // RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
         //LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getApplicationContext());
@@ -66,7 +69,7 @@ public class RouteActivity extends AppCompatActivity {
             for(int i = 0; i < mapid.length(); i++){
                 JSONObject temp = mapid.getJSONObject(i);
                 //Make all visited 0
-                visited.put(Integer.parseInt(temp.getString("ID")),0);
+                //visited.put(Integer.parseInt(temp.getString("ID")),0);
 
                 if(src.equals(temp.getString("Stations"))){
                     srcint=Integer.parseInt(temp.getString("ID"));
@@ -111,7 +114,7 @@ public class RouteActivity extends AppCompatActivity {
            e.printStackTrace();
         }
 
-        g.findpath(srcint,destint);
+        g.findpath(srcint,destint,shortestDist,finalPath);
 
 
 
@@ -162,7 +165,7 @@ public class RouteActivity extends AppCompatActivity {
 
         // Prints all paths from
         // 's' to 'd'
-        public void findpath(int s, int d) {
+        public void findpath(int s, int d, double sd, ArrayList<Integer> fp) {
             boolean[] isVisited = new boolean[v];
             ArrayList<Integer> pathList = new ArrayList<>();
 
@@ -170,7 +173,8 @@ public class RouteActivity extends AppCompatActivity {
             pathList.add(s);
 
             //Call recursive utility
-            printAllPathsUtil(s, d, isVisited, pathList);
+            printAllPathsUtil(s, d, isVisited, pathList,fp,sd);
+
         }
 
         // A recursive function to print
@@ -181,21 +185,51 @@ public class RouteActivity extends AppCompatActivity {
         // vertices in the current path
         private void printAllPathsUtil(Integer u, Integer d,
                                        boolean[] isVisited,
-                                       List<Integer> localPathList) {
+                                       List<Integer> localPathList, ArrayList<Integer> fp, Integer sd) {
 
             // Mark the current node
             isVisited[u] = true;
 
             if (u.equals(d)) {
+                double temp = 0;
+                int u1 = 0;
+                int v1 = 0;
+
 
                 //INCLUDE PATH STORING HERE
-               EditText test = (EditText) findViewById(R.id.testsrcint);
-                String path="";
-                for (int i =0; i<localPathList.size(); i++){
-                    path = path + " " + Integer.toString(localPathList.get(i));
+                //EditText test = (EditText) findViewById(R.id.testsrcint);
+                //String path="";
+                //for (int i =0; i<localPathList.size(); i++){
+                //    path = path + " " + Integer.toString(localPathList.get(i));
+                //}
+                //test.setText(path);
+
+                try {
+                    JSONObject obj = new JSONObject(loadJSONFromAsset("newmap.json"));
+                    JSONArray edgeArray = obj.getJSONArray("map");
+                    // implement for loop for getting users list data
+                    for (int j=0; j< localPathList.size()-1;j++ ) {
+                        u1= localPathList[j];
+                        v1= localPathList[j+1];
+                        for (int i = 0; i < edgeArray.length(); i++) {
+                            // create a JSONObject for fetching single user data
+
+                            JSONObject stationDetail = edgeArray.getJSONObject(i);
+                            // fetch email and name and store it in arraylist
+                            //stationList.add(Integer.parseInt(stationDetail.getString("ID")));
+                            //connectingList.add(Integer.parseInt(stationDetail.getString("Connecting")));
+                            //terminalList.add(Integer.parseInt(stationDetail.getString("Terminal")));
+                            //colorList.add(stationDetail.getString("Color"));
+                            //distanceList.add(Float.parseFloat(stationDetail.getString("Distance")));
+                            //Refer shanky ki copy
+
+                        }
+
+                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
                 }
-                test.setText(path);
-                
 
                 // System.out.println(localPathList);
                 // if match found then no need to traverse more till depth
